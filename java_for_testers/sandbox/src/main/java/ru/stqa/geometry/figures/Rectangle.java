a5dffd42ee8f6d9194f3684837a030e92c8897f0
Valiev Robert <valievrob@mail.ru>
2024-03-31 20:50:06 +0500

    Lecture

--- java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,22 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle (double a, double b) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(rectangle.a, this.a) == 0 && Double.compare(rectangle.b, this.b) == 0)
+                || (Double.compare(rectangle.a, this.b) == 0 && Double.compare(rectangle.b, this.a) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 
     public Rectangle {
         if (a < 0 || b < 0){
