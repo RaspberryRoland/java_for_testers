9bb82fcf5c822e13e2721a52ea5457253c9c1da4
Valiev Robert <valievrob@mail.ru>
2024-05-01 12:35:18 +0500

    Задание №13: Вынести описание тестового стенда в конфигурационный файл

--- java_for_testers/addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ java_for_testers/addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
 
     private LoginHelper session;
@@ -17,7 +19,10 @@ public class ApplicationManager {
 
     private ContactHelper contacts;
 
-    public void init(String browser) {
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if (browser.equals("Chrome")) {
                 driver = new ChromeDriver();
@@ -27,10 +32,10 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(825, 990));
             driver.findElement(By.name("user")).click();
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
